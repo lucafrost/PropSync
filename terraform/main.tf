aa4e27e97e90dc5ea84237a1fa55be6116acd888
Luca J. Frost <luca@lucafrost.com>
2025-03-26 14:42:54 +0400

    [tf] enable eventbridge

--- terraform/main.tf
+++ terraform/main.tf
@@ -128,17 +128,17 @@ resource "aws_lambda_function" "lambda" {
 }
 
 # EventBridge :: Create Target
-# resource "aws_cloudwatch_event_target" "target" {
-#   target_id = "PropSync-LambdaTarget"
-#   arn       = aws_lambda_function.lambda.arn
-#   rule      = aws_cloudwatch_event_rule.cron.name
-# }
+resource "aws_cloudwatch_event_target" "target" {
+  target_id = "PropSync-LambdaTarget"
+  arn       = aws_lambda_function.lambda.arn
+  rule      = aws_cloudwatch_event_rule.cron.name
+}
 
 # Lambda :: Grant EventBridge Permission
-# resource "aws_lambda_permission" "allow_eventbridge" {
-#   statement_id  = "AllowExecutionFromEventBridge"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.lambda.function_name
-#   principal     = "events.amazonaws.com"
-#   source_arn    = aws_cloudwatch_event_rule.cron.arn
-# }
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.cron.arn
+}
